f21cbeca77c3b72ab873996a8240ebba258d567c
Frederic Onana <onanafrederic31@gmail.com>
2025-04-24 22:42:29 +0100

    Update PipelineController.java

--- new_student_management/src/main/java/org/crm/student/application_management_service/controller/PipelineController.java
+++ new_student_management/src/main/java/org/crm/student/application_management_service/controller/PipelineController.java
@@ -2,7 +2,8 @@ package org.crm.student.application_management_service.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
+@CrossOrigin(origins = "*")
 @Controller
 public class PipelineController {
 
